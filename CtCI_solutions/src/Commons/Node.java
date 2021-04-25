4e4a3db9533bb737c93c2a3e1fc3f3ba065fcde1
ivan <ivanmoreno405@gmail.com>
2021-04-25 14:52:01 -0500

    added Node to use in a Linked List

---
+++ CtCI_solutions/src/Commons/Node.java
@@ -0,0 +1,29 @@
+package Commons;
+
+public class Node {
+
+	public Node next = null;
+	public int data;
+	
+	public Node (int d) {
+		data = d;
+	}
+	
+	void appendToTail(int d) {
+		Node end = new Node (d);
+		Node n = this;
+		while(n.next != null) {
+			n = n.next;
+		}
+		n.next = end;
+	}
+	
+	public Node(int[] array) {
+        data = array[0];
+        Node current = this;
+        for (int i = 1; i < array.length; i++) {
+        	current.next = new Node(array[i]);
+        	current = current.next;
+        }
+    }
+}
