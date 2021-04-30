ac90b6068d0b0d55d68d6e5ac8a65a6a099a0033
ivan <ivanmoreno405@gmail.com>
2021-04-30 18:49:35 -0500

    update Sum Lists with Follow up

--- CtCI_solutions/src/Chapter2/Q2_5_SumLists.java
+++ CtCI_solutions/src/Chapter2/Q2_5_SumLists.java
@@ -1,5 +1,7 @@
 package Chapter2;
 
+import java.util.Stack;
+
 import Commons.ListFunctions;
 import Commons.Node;
 
@@ -42,6 +44,27 @@ public class Q2_5_SumLists {
 		ListFunctions.printList(sumReverseList(list5, list6));
 		System.out.println("---------------------------");
 		
+		System.out.println("\n------------ FOLLOW UP ---------------");
+		System.out.println("--------------------------------------\n");
+		
+		Node list7 = new Node(new int [] {6, 1, 7, 9});
+		Node list8 = new Node(new int [] {2, 9, 5});
+		
+		ListFunctions.printList(sumForwardList(list7, list8));
+		System.out.println("---------------------------");
+		
+		Node list9 = new Node(new int [] {6, 1, 7});
+		Node list10 = new Node(new int [] {2, 9, 5});
+		
+		ListFunctions.printList(sumForwardList(list9, list10));
+		System.out.println("---------------------------");
+		
+		Node list11 = new Node(new int [] {6, 9, 6});
+		Node list12 = new Node(new int [] {2, 9, 3, 3, 3});
+		
+		ListFunctions.printList(sumForwardList(list11, list12));
+		System.out.println("---------------------------");
+		
 	}
 	
 	public static Node sumReverseList(Node l1, Node l2) {
@@ -71,5 +94,65 @@ public class Q2_5_SumLists {
 		}
 		
 		return resultList.next;
+	}
+	
+	/**
+	 * 	.
+	 *  6 -> 1 -> 7 -> 9 -> null
+	 *  .   
+	 *  2 -> 9 -> 5 -> null
+	 *  
+	 *  
+	 *  Stack1 [6  ]
+	 *  Stack2 []
+	 *  
+	 *  carry = 0
+	 *  newDigit = 6
+	 *  
+	 * 	6 -> 4 -> 7 -> 4
+	 *  
+	 *  .       .      .     .      .
+	 *  6 ->   4 ->  7 -> 4 -> null
+	 * 
+	 * */
+	
+	public static Node sumForwardList(Node l1, Node l2) {
+		
+		Stack<Integer> stack1 = linkedListInStack(l1);
+		Stack<Integer> stack2 = linkedListInStack(l2);
+		
+		int carry = 0;
+		int newDigit = 0;
+		
+		Node resultList = null;
+		
+		while((!stack1.isEmpty()) || (!stack2.isEmpty()) || carry!= 0) {
+			
+			if(!stack1.isEmpty()) {
+				carry += stack1.pop();
+			}
+			if(!stack2.isEmpty()) {
+				carry += stack2.pop();
+			}
+			
+			newDigit = carry % 10; 
+			carry = carry / 10; 
+			
+			Node n = new Node(newDigit);
+			n.next = resultList;
+			resultList = n;
+		}
+		
+		return resultList;
+	}
+	
+	public static Stack<Integer> linkedListInStack(Node n){
+		Stack<Integer> stack = new Stack<>();
+		
+		while(n != null) {
+			stack.add(n.data);
+			n = n.next;
+		}
+		return stack;
 	}
 }
